bc78237f873a4c65339fe0139eecd2ef3f84d8be
deemoji <diman.martyanov@yandex.ru>
2024-08-12 17:13:10 +0300

    Initial Commit

---
+++ DemiurgeTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemiurgeTask
+//
+//  Created by Дмитрий Мартьянов on 12.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
